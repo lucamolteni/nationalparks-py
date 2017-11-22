a8b8ef411a8b900ddd742ffd021bbd82e65de560
Luca Molteni <volothamp@gmail.com>
2017-11-22 15:51:04 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -31,7 +31,7 @@ api.add_resource(HealthCheck, '/ws/healthz/')
 
 class Info(Resource):
     description = {
-        'id': 'nationalparks-py',
+        'id': 'nationalparks-py Pippo',
         'displayName': 'National Parks',
         'type': 'cluster',
         'center': {'latitude': '47.039304', 'longitude': '14.505178'},
